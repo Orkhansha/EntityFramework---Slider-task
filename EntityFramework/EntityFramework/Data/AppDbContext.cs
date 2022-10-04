16e859aa9a41e7235b51715b158635c11eaed23c
Orkhansha <orkhansha@code.edu.az>
2022-10-05 03:43:02 +0400

    Added Entity framework task files

---
+++ EntityFramework/EntityFramework/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EntityFramework.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SliderDetail> SliderDetails { get; set; }
+
+    }
+}
